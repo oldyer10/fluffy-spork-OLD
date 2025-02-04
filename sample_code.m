42bc27ec00a7fbdf53092727f958531a0cc1aa4c
oldyer10 <oldyer@udel.edu>
2025-02-03 20:06:22 -0500

    adding new plot

--- sample_code.m
+++ sample_code.m
@@ -35,3 +35,46 @@ legend('Pavement', 'Sidewalk', 'Grass', 'Mulch', ...
 box off
 set(gca,"TickDir","out","XColor",[0 0 0], "YColor",[0 0 0],"LineWidth",2,"XMinorTick","off","YMinorTick","off");
 set(gca,"FontSize",18,"FontWeight","bold"); % Changing font style
+
+
+%%
+
+figure (2)
+% Create data and 2-by-1 tiled chart layout
+x = linspace(0,2*pi);
+y1 = cos(-2*x +pi) + 1;
+y2 = 2*cos(-5*x + pi) + 2;
+tiledlayout(1,2)
+
+% Top plot
+ax1 = nexttile;
+p1 = plot(ax1,x,y1);
+
+p1.LineWidth = 2;
+p1.Color = '#648fff';
+p1.Marker = '*';
+
+title(ax1,'Me Jumping w/o Caffeine')
+ylabel(ax1,'Height Off Ground (ft)')
+ylim([0 4])
+
+box off
+set(gca,"TickDir","out","XColor",[0 0 0], "YColor",[0 0 0],"LineWidth",2,"XMinorTick","off","YMinorTick","off");
+set(gca,"FontSize",18,"FontWeight","bold"); % Changing font style
+
+% Bottom plot
+ax2 = nexttile;
+p2 = plot(ax2,x,y2);
+
+p2.LineWidth = 2;
+p2.Color = '#dc267f';
+p2.LineStyle = '-.';
+
+
+title(ax2,'Me Jumping w/ Caffeine')
+ylabel(ax2,'Hight Off Ground (ft)')
+
+
+box off
+set(gca,"TickDir","out","XColor",[0 0 0], "YColor",[0 0 0],"LineWidth",2,"XMinorTick","off","YMinorTick","off");
+set(gca,"FontSize",18,"FontWeight","bold"); % Changing font style
